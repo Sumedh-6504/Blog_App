31989289a6ed8b76d13d6ed9f94e80bc72941639
Sumedh-R-Mundewadi <sumedhrm.gdsc@gmail.com>
2025-11-26 19:53:41 +0530

    Completed the backend

--- app/db.py
+++ app/db.py
@@ -6,21 +6,28 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey # pyright: ign
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker # pyright: ignore[reportMissingImports]
 from sqlalchemy.dialects.postgresql import UUID # pyright: ignore[reportMissingImports]
 from sqlalchemy.orm import DeclarativeBase, relationship # pyright: ignore[reportMissingImports]
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID # pyright: ignore[reportMissingImports]
+from fastapi import Depends # pyright: ignore[reportMissingImports]
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
 class Base(DeclarativeBase):
     pass
 
+class User(Base, SQLAlchemyBaseUserTableUUID):
+    posts = relationship("Post", back_populates="user")
+
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
     caption = Column(Text)
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=lambda: datetime.utcnow()) # pyright: ignore[reportAttributeAccessIssue]
+    user = relationship("User", back_populates="posts")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
@@ -32,3 +39,6 @@ async def create_db_and_tables():
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel #type: ignore
+import uuid
+from fastapi_users import schemas
 
 class PostSchema(BaseModel):
     title: str
@@ -7,3 +9,12 @@ class PostSchema(BaseModel):
 class PostResponse(BaseModel):
     title: str
     content: str
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
